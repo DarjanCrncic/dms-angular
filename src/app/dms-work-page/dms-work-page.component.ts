cafc1c138ae9703c427c8f77db48fd1618732e02
DarjanCrncic <darjancr@gmail.com>
2022-09-24 17:51:21 +0200

    Resizing improvements.

--- src/app/dms-work-page/dms-work-page.component.ts
+++ src/app/dms-work-page/dms-work-page.component.ts
@@ -1,19 +1,55 @@
-import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
-import { Subject, Subscription, takeUntil } from 'rxjs';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild
+} from '@angular/core';
+import {
+  fromEvent,
+  Subject,
+  Subscription,
+  switchMapTo,
+  takeUntil,
+  tap
+} from 'rxjs';
 import { SidebarService } from '../shared/services/sidebar-service';
 @Component({
   selector: 'app-dms-work-page',
   templateUrl: './dms-work-page.component.html',
   styleUrls: ['./dms-work-page.component.css']
 })
-export class DmsWorkPageComponent implements OnInit, OnDestroy {
+export class DmsWorkPageComponent implements OnInit, OnDestroy, AfterViewInit {
   opened = true;
   sidebarSubscription = new Subscription();
   navWidth = 400;
   resizing = false;
   private $componentDestroyed = new Subject();
 
+  @ViewChild('mover') mover: ElementRef<HTMLElement> = {} as ElementRef;
   constructor(private sidebarService: SidebarService) {}
+
+  ngAfterViewInit(): void {
+    const element = this.mover.nativeElement;
+    const mousedown$ = fromEvent(element, 'mousedown');
+    const mousemove$ = fromEvent(document, 'mousemove');
+    const mouseup$ = fromEvent(document, 'mouseup');
+
+    mousedown$
+      .pipe(
+        switchMapTo(
+          mousemove$.pipe(
+            tap((event: any) => {
+              this.sidebarService.setSidebarWidth(event.clientX);
+            }),
+            takeUntil(mouseup$)
+          )
+        ),
+        takeUntil(this.$componentDestroyed)
+      )
+      .subscribe();
+  }
 
   ngOnInit(): void {
     this.sidebarService.$toggleSidebar
@@ -33,20 +69,5 @@ export class DmsWorkPageComponent implements OnInit, OnDestroy {
 
   onToggleEvent() {
     this.sidebarService.toggle();
-  }
-
-  @HostListener('window:mousemove', ['$event'])
-  onMouseMove(event: MouseEvent) {
-    if (this.resizing) {
-      this.sidebarService.setSidebarWidth(event.clientX);
-    }
-  }
-
-  setResizing() {
-    this.resizing = true;
-  }
-
-  resetResizing() {
-    this.resizing = false;
   }
 }
